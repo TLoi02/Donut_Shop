84ec4f8ca7f75faa434f08b7111a66f387555bad
TLoi02 <vothuanloi110802@gmail.com>
2023-05-04 17:14:54 +0700

    Update getProduct.js

--- module_Function/getProduct.js
+++ module_Function/getProduct.js
@@ -1,5 +1,5 @@
 function getProduct (callback){
-    fetch('../JSON_DATA/product.json')
+    fetch('https://my-json-server.typicode.com/TLoi02/JSON_API/product')
         .then(response => response.json())
         .then(callback)
 }
